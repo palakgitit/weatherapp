623960960b08383ec485ac8b4618c100f7538dcb
palakgitit <palkzzzy@gmail.com>
2026-01-06 02:00:09 +0530

    commit

--- asset/js/weather.js
+++ asset/js/weather.js
@@ -1,8 +1,7 @@
-
 const  form = document.querySelector("form");
 const city = document.getElementById("cityEntered");
 const displayData=document.getElementById("displayData");
-
+const dayData = document.getElementById("dayData"); 
 
 
 form.addEventListener("submit", function (btn) {
@@ -17,6 +16,7 @@ form.addEventListener("submit", function (btn) {
     }
 
     weatherData(cityName);
+    futureData(cityName);
 
 });
 
@@ -31,12 +31,37 @@ async function weatherData(cityName) {
     displayData.innerHTML = `
 
         <li class="mb-1">${data.location.name} </li>
-        <li class="mb-1">${data.current.temp_c}c* / ${data.current.temp_f}f*</li>
+        <li class="mb-1">${data.current.temp_c}°C/${data.current.temp_f}°F</li>
         <li class="mb-1">${data.current.humidity}%</li>
         <li class="mb-1">${data.current.wind_kph}km/h</li>
         <li class="mb-1">${data.current.condition.text}</li>
 `;   
 }
+
+async function futureData(cityName) {
+
+    const futureapiKey = "1eb2a89d353047cba90171320253112";
+    const futureurl = `https://api.weatherapi.com/v1/forecast.json?key=${futureapiKey}&q=${cityName}&days=4&aqi=no`;
+
+    const response = await fetch(futureurl);
+    const data = await response.json();
+
+    dayData.innerHTML = ""; 
+
+    data.forecast.forecastday.forEach(day => {
+        dayData.innerHTML += `
+            <div class="blueDatabox ">
+                <h6>${new Date(day.date).toLocaleDateString("en-US", {
+                    weekday: "short"
+                })}</h6>
+                <span>${day.day.avgtemp_c}°C</span>
+            </div>
+        `;
+    });
+    
+}
+
+
 
 
 
